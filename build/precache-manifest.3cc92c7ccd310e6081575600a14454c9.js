58548d8480f9017129da7f5302a6d4e8c1975c8b
Christopher Brenberg <cbrenberg@gmail.com>
2018-11-28 15:47:21 -0600

    create production build

---
+++ build/precache-manifest.3cc92c7ccd310e6081575600a14454c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f18503cbd78cbfa3f0b",
+    "url": "/static/js/main.2f18503c.chunk.js"
+  },
+  {
+    "revision": "1701867f019ab1f416a2",
+    "url": "/static/css/1.87e7d73e.chunk.css"
+  },
+  {
+    "revision": "1701867f019ab1f416a2",
+    "url": "/static/js/1.1701867f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "702da639090d0bf2d64cd852b7bca550",
+    "url": "/index.html"
+  }
+];
